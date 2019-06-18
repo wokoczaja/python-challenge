b7071b40a7c3de695291c91d7c9b91d6a2f71c90
Laura Koczaja <wokoczaja@gmail.com>
2019-06-18 17:58:42 -0500

    Uploading homework

---
+++ Practice Python.py
@@ -0,0 +1,13 @@
+#print("Hello User")
+#name=input("What is your name?")
+#print("Hello" + name)
+#print("What is your age?")
+#y=input()
+#if((int(y)<=40):
+#    print("Awwwww you're just a baby")
+#else:
+#    print("A well traveled soul are ye")
+print("Welcome to the House of Pies!  Here are our pies:")
+pielist=["Pecan", "Apple Crisp", "Bean", "Banoffee", "Black Bun", "Blueberry","Tamale"]
+for x in pielist:
+    print(x)
